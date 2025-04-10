3fb41716a35daf317dafe5519a6de3b0b997c1c5
Eduardo Jaime <eduardojaime86@gmail.com>
2025-04-09 21:23:48 -0400

    Updated Final Exam

---
+++ DotNetDrinksWebUI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DotNetDrinksWebUI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetDrinksWebUI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ DotNetDrinksWebUI/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetDrinksWebUI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // navigation property to child Product objects
+        public List<Product>? Products { get; set; }
+    }
+}
